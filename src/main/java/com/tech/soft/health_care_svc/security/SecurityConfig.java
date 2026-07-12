b08057b54fe9a81b476c6b13337f7cb7955abb59
Suraj Singh <kumarsurajsingh93@gmail.com>
2026-07-12 19:02:56 +0530

    cross origin fixed for ip

--- src/main/java/com/tech/soft/health_care_svc/security/SecurityConfig.java
+++ src/main/java/com/tech/soft/health_care_svc/security/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://192.168.1.11:5173"));
 
         configuration.setAllowedMethods(List.of(
                 "GET",
